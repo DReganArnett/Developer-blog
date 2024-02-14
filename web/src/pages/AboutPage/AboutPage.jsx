a81470a648fe3d57a9ae76f175f6b580105b8d54
Danielle Regan Arnett <126263892+DReganArnett@users.noreply.github.com>
2024-02-14 06:49:46 -0700

    Initial commit 2

---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,15 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+      <p>
+        All about Redwood
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import { Metadata } from '@redwoodjs/web'
+
+import ArticlesCell from 'src/components/ArticlesCell'
+
+const HomePage = () => {
+  return (
+    <>
+      <ArticlesCell />
+    </>
+  )
+}
+
+export default HomePage
